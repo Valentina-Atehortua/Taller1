e0bb99497a20c05c4bf1cd74975f8da4171ad3a0
Valentina-Atehortua <vatehortua228@gmail.com>
2025-05-12 12:49:57 -0500

    a

---
+++ js/triangulo.js
@@ -0,0 +1,4 @@
+let A =parseInt(prompt("Digita el primer lado"))
+let B =parseInt(prompt("Digita el segundo lado"))
+H = Math.sqrt(A**2 + B**2)
+document.writeln("La potencia de", A, "+", B, "la hipotenusa es", H )
