18d1d5a73152f33e876054d187fcf8bbebe116f1
Valentina-Atehortua <vatehortua228@gmail.com>
2025-05-12 12:22:21 -0500

    primer commit

---
+++ js/clase1.js
@@ -0,0 +1,31 @@
+/* var numero1 = 5
+let num2= 2
+console.log(num2)
+const pi = 3.14
+
+num2 = 10
+console.log(num2) */
+let A = 24
+let B = 10 
+console.log("la suma de", A, "+", B, "=", A + B)
+/* let suma = 0
+let resta = 0
+let multiplicacion = 0
+let division = 0
+
+suma = A + B
+resta = A - B
+multiplicacion = A * B
+division = A / B
+
+console.log("la suma de los vlore es: ",suma)
+console.log("la suma de ", A, " + ", B, " es igual a ",suma)
+
+console.log("la resta de los vlore es: ",resta)
+console.log("la resta de ", A, " - ", B, " es igual a ",resta)
+
+console.log("la multiplicacion de los vlore es: ",multiplicacion)
+console.log("la multiplicacion de ", A, " * ", B, " es igual a ",multiplicacion)
+
+console.log("la division de los vlore es: ",division)
+console.log("la division de ", A, " / ", B, " es igual a ",division) */
---
+++ js/clase2.js
@@ -0,0 +1,13 @@
+/* let nombre = prompt("digita tu nombre: ")
+console.log("el nombre es: ", nombre)
+
+document.writeln(nombre) */
+/* let A =parseInt(prompt("digite el primer valor"))
+let B = parseInt(prompt("digite el segundo valor"))
+document.writeln(A-B) */
+
+let A = parseFloat(prompt("Digite la primera nota"))
+let B = parseFloat(prompt("Digite la segunda nota"))
+let C = parseFloat(prompt("Digite la tercera nota"))
+
+document.writeln((A+B+C)/(3))
